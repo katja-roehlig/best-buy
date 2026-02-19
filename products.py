ccba47ff2e7f41723b9816401f014b3c93e14581
Katja Röhlig <katja-roehlig@mail.de>
2026-02-19 12:07:57 +0100

    create product class

---
+++ products.py
@@ -0,0 +1,53 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if not name.strip():
+            raise ValueError("Productname cannot be empty.")
+        if not isinstance(price, (int, float)):
+            raise ValueError("Price has to be a number.")
+        if price <= 0:
+            raise ValueError("Price cannot be 0 or negative.")
+
+        self.validate_quantity(quantity)
+        self._name = name
+        self._price = price
+        self._quantity = quantity
+        self._activate = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.validate_quantity(quantity)
+        self._quantity += quantity
+
+    def is_active(self):
+        return self._activate
+
+    def activate(self):
+        self._activate = True
+
+    def deactivate(self):
+        self._activate = False
+
+    def show(self):
+        print(f"{self._name}, Price: {self._price}, Quantity: {self._quantity} ")
+
+    def buy(self, quantity) -> float:
+        self.validate_quantity(quantity)
+        if self._quantity >= quantity:
+            total_price = self._price * quantity
+            if self._quantity - quantity <= 0:
+                self.deactivate()
+                self._quantity = 0
+            else:
+                self._quantity = self._quantity - quantity
+        else:
+            raise ValueError("Not enough products available")
+        return total_price
+
+    @staticmethod
+    def validate_quantity(quantity):
+        if not isinstance(quantity, (int)):
+            raise ValueError("Quantity has to be an integer.")
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
